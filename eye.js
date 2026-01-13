277277cb50c87abd64f3072d496b10f4d1231348
Seb Zanardo <sebzanardo@optusnet.com.au>
2026-01-13 18:16:03 +1100

    SVG logo text lines up with corner of eye. Reduced size. Fixed double blink. Blinking more rare

--- eye.js
+++ eye.js
@@ -4,13 +4,17 @@ const eyelid = document.getElementById('eyelid');
 const EYELID_OPEN_VALUE = "M 0,0 V 2048 H 2048 V 0 Z m 1024,448 c 639.9995,0 896,768 896,768 0,0 -384.0004,384 -896,384 -511.99943,0 -896,-384 -896,-384 0,0 256.0007,-768 896,-768 z";
 const EYELID_CLOSED_VALUE = "M 1.6975098e-6,1.9970703e-6 V 2048 H 2048 V 1.9970703e-6 Z M 1034.1337,1471.8728 C 1674.1332,1471.8728 1920,1216 1920,1216 c 0,0 -384.0004,384 -896,384 -511.9994,0 -896,-384 -896,-384 0,0 266.1344,255.8728 906.1337,255.8728 z";
 
-const MIN_DELAY_MS = 3500;
-const MAX_DELAY_MS = 8000;
+const MIN_DELAY_MS = 8000;
+const MAX_DELAY_MS = 20000;
 const OPEN_DURATION_MS = 200;
 const CLOSED_DURATION_MS = 100;
-const DOUBLE_BLINK_CHANCE = 0.2;
+const DOUBLE_BLINK_CHANCE = 0.4;
 
 let user_holding = false;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function eyeOpen() {
     eyelid.style.transition = `d ${OPEN_DURATION_MS}ms ease-in-out`;
@@ -26,17 +30,22 @@ function eyeBlink() {
     if (user_holding) return;
     setTimeout(() => {
         eyeClose();
-        setTimeout(eyeOpen, OPEN_DURATION_MS);
+        setTimeout(() => {
+            eyeOpen();
+        }, OPEN_DURATION_MS);
     }, CLOSED_DURATION_MS);
 }
 
-function eyeRandomBlink() {
-    let delay = Math.random() * (MAX_DELAY_MS - MIN_DELAY_MS) + MIN_DELAY_MS;
-    setTimeout(() => {
+async function eyeRandomBlink() {
+    while (true) {
+        let delay = Math.random() * (MAX_DELAY_MS - MIN_DELAY_MS) + MIN_DELAY_MS;
+        await sleep(delay);
         eyeBlink();
-        if (Math.random() < DOUBLE_BLINK_CHANCE) { eyeBlink(); }
-        eyeRandomBlink();
-    }, delay);
+        if (Math.random() < DOUBLE_BLINK_CHANCE) {
+            await sleep(CLOSED_DURATION_MS + OPEN_DURATION_MS);
+            eyeBlink();
+        }
+    }
 }
 
 eye.addEventListener('mousedown', () => {
@@ -58,4 +67,5 @@ eye.addEventListener('mouseleave', () => {
     }
 });
 
+eyeBlink();
 eyeRandomBlink();
